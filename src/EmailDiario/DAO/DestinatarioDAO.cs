3858cf2ea6cf832985e3bd755b096ce5dcba5d53
Lucas Mendes <lcmendes1994@gmail.com>
2026-09-26 12:44:33 -0300

    DestinatarioDAO.cs feito

--- src/EmailDiario/DAO/DestinatarioDAO.cs
+++ src/EmailDiario/DAO/DestinatarioDAO.cs
@@ -1,4 +1,6 @@
 ﻿using EmailDiario.Data;
+using EmailDiario.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmailDiario.DAO
   
@@ -11,5 +13,13 @@ namespace EmailDiario.DAO
         {
             _context = context;
         }
+        public async Task<List<Destinatario>> ListarTodosAsync()
+        {
+            return await _context.Destinatarios
+                .AsNoTracking()
+                .OrderBy(d => d.Nome)
+                .ToListAsync();
+        }
     }
+
 }
