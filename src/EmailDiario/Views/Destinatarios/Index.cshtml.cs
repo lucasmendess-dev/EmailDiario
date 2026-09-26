ba0c7b35ec6b20508593a326f313cdb9148cdf30
Lucas Mendes <lcmendes1994@gmail.com>
2026-09-26 12:54:37 -0300

    Atualização2

---
+++ src/EmailDiario/Views/Destinatarios/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EmailDiario.Views.Destinatarios
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
